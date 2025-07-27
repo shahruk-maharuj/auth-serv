b947fc5861d3fb225de5deaaee28d57cbf059fb4
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2025-07-27 09:18:20 +0600

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 function welcome(name: string) {
-  console.log(`Welcome, ${name}!`);
+    console.log('Hello');
+    const user = {
+        name: 'Shahruk',
+    }
+    const u1 = user.name;
+    return `Welcome ${u1} ${name} to the world of TypeScript!`;
+
 }
 
 welcome('Shahruk');
