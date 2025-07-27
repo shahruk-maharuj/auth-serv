4912936d12309585cb3fce1671fb4868c5068718
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2025-07-27 10:03:56 +0600

    test lintstaged 2

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 function welcome(name: string) {
-    console.log('Hello');
-    const user = {
-        name: 'Shahruk',
-    }
-    const u1 = user.name;
-    return `Welcome ${u1} ${name} to the world of TypeScript!`;
+  console.log('Hello');
+  const user = {
+    name: 'Shahruk',
+  };
 
+  const fname = user.name;
+
+  return name + fname;
 }
 
 welcome('Shahruk');
