21d7b1815165f3914a03ea96d6b5fd5722771ab1
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2025-07-28 11:41:40 +0600

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import app from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 const startServer = () => {
   const PORT = Config.port;
   try {
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      logger.warn('testing warning message...');
+      logger.error('testing error message...');
+      logger.debug('testing debug message...');
+      logger.info(`Server is running on port ${PORT}`);
     });
   } catch (err) {
-    console.error('Error starting server:', err);
+    logger.error('Error starting server:', err);
     process.exit(1);
   }
 };
