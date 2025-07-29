fd27bd447d4a1a7465a1ac9ed905dcbf364225cb
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2025-07-29 07:56:00 +0600

    add test setup

---
+++ app.spec.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import app from './src/app';
+import { calculateDiscount } from './src/utils';
+
+describe('App', () => {
+  it('should return correct discount amount', () => {
+    const discount = calculateDiscount(100, 20);
+    expect(discount).toBe(20);
+  });
+
+  it('should return 200 status code', async () => {
+    const response = await request(app).get('/').send();
+    expect(response.statusCode).toBe(200);
+  });
+});
