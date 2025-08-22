ce393f6377c5110c47ca3bff965a20d3e98b1eb7
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2025-08-22 09:37:06 +0600

    add access middleware

---
+++ src/middlewares/canAccess.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import createHttpError from 'http-errors';
+import { AuthRequest } from '../types';
+
+export const canAccess = (roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const _req = req as AuthRequest;
+    const roleFromToken = _req.auth.role;
+
+    if (!roles.includes(roleFromToken)) {
+      const error = createHttpError(
+        403,
+        "You don't have permission to access this resource",
+      );
+      return next(error);
+    }
+
+    next();
+  };
+};
